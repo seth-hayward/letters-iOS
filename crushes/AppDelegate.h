0dc81e372018ab2ccf676c94bb591357fbf91e63
Seth Hayward <seth.hayward@gmail.com>
2012-12-06 05:27:47 -0500

    Initial Commit

---
+++ crushes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  crushes
+//
+//  Created by Seth Hayward on 12/6/12.
+//  Copyright (c) 2012 Seth Hayward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
