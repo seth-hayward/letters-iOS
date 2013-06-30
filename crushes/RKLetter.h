02a9ee2b965c291df1002a9eb0fe4ca9a1b7425c
Seth Hayward <seth.hayward@gmail.com>
2013-06-30 14:45:39 -0700

    setting up objects for posting to website api

---
+++ crushes/RKLetter.h
@@ -0,0 +1,19 @@
+//
+//  RKLetter.h
+//  crushes
+//
+//  Created by Seth Hayward on 6/30/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RKLetter : NSObject
+{
+}
+
+- (id)initWithLetterMessage:(NSString *)message;
+
+@property (nonatomic, copy) NSString *letterMessage;
+
+@end
---
+++ crushes/RKLetter.m
@@ -0,0 +1,31 @@
+//
+//  RKLetter.m
+//  crushes
+//
+//  Created by Seth Hayward on 6/30/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "RKLetter.h"
+
+@implementation RKLetter
+@synthesize letterMessage;
+
+- (id)initWithLetterMessage:(NSString *)message
+{
+    self = [super init];
+    
+    if (self) {
+        [self setLetterMessage:message];
+    }
+    
+    return self;
+}
+
+- (id)init
+{
+    return [self initWithLetterMessage:@" "];
+}
+
+@end
+
