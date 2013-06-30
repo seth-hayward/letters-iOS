2df6b97a2fc6576e9dc6009215e05c1a9ea6bb09
Seth Hayward <seth.hayward@gmail.com>
2013-06-30 16:51:21 -0700

    trying to add send support

--- crushes/SendViewController.h
+++ crushes/SendViewController.h
@@ -14,5 +14,6 @@
     __weak IBOutlet UIButton *sendButton;    
 }
 - (IBAction)backgroundTapped:(id)sender;
+- (IBAction)sendLetter:(id)sender;
 
 @end
--- crushes/SendViewController.m
+++ crushes/SendViewController.m
@@ -8,6 +8,7 @@
 
 #import "SendViewController.h"
 #import <QuartzCore/QuartzCore.h>
+#import "RKLetter.h"
 
 @implementation SendViewController
 
@@ -20,6 +21,16 @@
         [tbi setTitle:@"Send"];
         [tbi setImage:[UIImage imageNamed:@"pencil.png"]];
     }
+    
+    NSLog(@"Hello from SendView.");
+    
+    NSURL *baseURL = [NSURL URLWithString:@"http://www.letterstocrushes.com/"];  //http://example.com/v1/"];
+    [NSURL URLWithString:@"foo" relativeToURL:baseURL];                  // http://example.com/v1/foo
+    [NSURL URLWithString:@"foo?bar=baz" relativeToURL:baseURL];          // http://example.com/v1/foo?bar=baz
+    [NSURL URLWithString:@"/foo" relativeToURL:baseURL];                 // http://example.com/foo
+    [NSURL URLWithString:@"foo/" relativeToURL:baseURL];                 // http://example.com/v1/foo
+    [NSURL URLWithString:@"/foo/" relativeToURL:baseURL];                // http://example.com/foo/
+    [NSURL URLWithString:@"http://example2.com/" relativeToURL:baseURL]; // http://example2.com/
     
     return self;
 }
@@ -44,4 +55,24 @@
 - (IBAction)backgroundTapped:(id)sender {
     [[self view] endEditing:YES];
    }
+
+- (IBAction)sendLetter:(id)sender {
+    // Read the value from the text field
+	NSString *letter_message = [messageText text];
+    
+	// Create a new letter and POST it to the server
+	RKLetter* letter = [RKLetter new];
+	letter.letterText = letter_message;
+    letter.letterCountry = @" ";
+    
+	[[RKObjectManager sharedManager] postObject:letter path:@"http://www.letterstocrushes.com/Home/Mail" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+        UIAlertView *alert_success = [[UIAlertView alloc] initWithTitle:@"Success!" message: @"It was sent." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+        [alert_success show];
+    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Send Error" message: [error description] delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+        [alert show];        
+    }];
+    
+    NSLog(letter_message);
+}
 @end
