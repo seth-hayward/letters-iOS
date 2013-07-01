83e77d179409fd71555f307f2e5d262aeebd2b22
Seth Hayward <seth.hayward@gmail.com>
2013-07-01 09:20:23 -0700

    successfully reading the response now, but not posting the objects again

--- crushes/SendViewController.m
+++ crushes/SendViewController.m
@@ -63,9 +63,8 @@
     
 	// Create a new letter and POST it to the server
 	RKLetter* letter = [RKLetter new];
-	letter.lettertext = letter_message;
-    letter.lettercountry = @" ";
-
+	letter.letterText = letter_message;
+    letter.letterCountry = @"US";
     
     NSURL *baseURL = [NSURL URLWithString:@"http://www.letterstocrushes.com"];
     AFHTTPClient *client = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
@@ -74,34 +73,57 @@
     
     RKObjectManager *objectManager = [[RKObjectManager alloc] initWithHTTPClient:client];
     
-    RKObjectMapping *requestMapping = [RKObjectMapping requestMapping];
-    [requestMapping addAttributeMappingsFromArray:@[@"message", @"response", @"guid"]];
+//    RKObjectMapping *requestMapping = [RKObjectMapping requestMapping];
+//    [requestMapping addAttributeMappingsFromArray:@[@"message", @"response", @"guid"]];
+//    
+//    RKRequestDescriptor *requestDescriptor = [RKRequestDescriptor requestDescriptorWithMapping:requestMapping objectClass:[RKMessage class] rootKeyPath:@""];
+//    
+//    [objectManager addRequestDescriptor:requestDescriptor];
     
-    RKRequestDescriptor *requestDescriptor = [RKRequestDescriptor requestDescriptorWithMapping:requestMapping objectClass:[RKMessage class] rootKeyPath:@""];
     
-    [objectManager addRequestDescriptor:requestDescriptor];
+    // begin github sample
     
+    RKObjectMapping* responseObjectMapping = [RKObjectMapping mappingForClass:[RKMessage class]];
+    [responseObjectMapping addAttributeMappingsFromDictionary:@{
+        @"response": @"response",
+        @"message": @"message",
+        @"guid": @"guid"
+     }];
     
-    NSString *url = [NSString stringWithFormat:@"%@%@%@", @"/Home/Mail?letterText=", letter_message, @"&letterCountry=US"];
+    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:responseObjectMapping pathPattern:nil keyPath:nil statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+    
+    NSString *url = [NSString stringWithFormat:@"%@%@%@", @"http://www.letterstocrushes.com/Home/Mail?letterText=", letter_message, @"&letterCountry=US"];
     NSString *safe_url = [url stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+    NSString *real_url = @"http://www.letterstocrushes.com/home/mail";
     
-    [objectManager postObject:nil path:safe_url parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-        UIAlertView *alert_success = [[UIAlertView alloc] initWithTitle:@"Success!" message: @"It was sent." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
-        [alert_success show];
+    [objectManager addResponseDescriptor:responseDescriptor];
+    
+    [objectManager postObject:letter path:real_url parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+        
+        // now we just need to check the response
+        // there may have been an error on the server that
+        // we want to check for
+        RKMessage* msg = mappingResult.array[0];
+       
+        if([msg.response isEqualToNumber:[NSNumber numberWithInt:0]]) {
+            // there was an error on the server
+            
+            UIAlertView *alert_error = [[UIAlertView alloc] initWithTitle:@"Error" message:msg.message delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+            [alert_error show];
+            
+        } else {
+            // we good
+            UIAlertView *alert_success = [[UIAlertView alloc] initWithTitle:@"Success!" message: @"It was sent." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+            [alert_success show];
+            
+        }
     } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Send Error" message: [error description] delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
-        [alert show];
+        
+        // this occurs when restkit can not send a post -- this could happen
+        // if the user does not have internet connection at the time
+        UIAlertView *alert_post_error = [[UIAlertView alloc] initWithTitle:@"iOS Post Error" message: [error description] delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+        [alert_post_error show];
     }];
     
-//
-//    [objectManager postObject:letter path:@"/home/mail" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-//        UIAlertView *alert_success = [[UIAlertView alloc] initWithTitle:@"Success!" message: @"It was sent." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
-//        [alert_success show];
-//    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Send Error" message: [error description] delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
-//        [alert show];
-//    }];
- 
-    NSLog(@"hi");
 }
 @end
