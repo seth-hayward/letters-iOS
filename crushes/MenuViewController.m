cf83a2395df3d580b7887203bbcc51c2cbb137c6
Seth Hayward <seth.hayward@gmail.com>
2013-07-22 12:15:48 -0700

    using a tableview controller for the menu

--- crushes/MenuViewController.m
+++ crushes/MenuViewController.m
@@ -8,31 +8,19 @@
 
 #import "MenuViewController.h"
 
-@interface MenuViewController ()
-
-@end
-
 @implementation MenuViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+- (id) init {
+    self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
 
-- (void)viewDidLoad
+-(id) initWithStyle:(UITableViewStyle)style
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return [self init];
 }
 
 @end
