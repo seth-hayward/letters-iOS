63598f13482e06cf00bdd6116bc6e3e85f140df5
Seth Hayward <seth.hayward@gmail.com>
2013-07-23 12:07:45 -0700

    setting up references for login view on menu controller

--- crushes/MenuViewController.h
+++ crushes/MenuViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface MenuViewController : UITableViewController <UITableViewDelegate>
+{
+    IBOutlet UIView *loginView;
+}
+
+- (UIView *)loginView;
+- (IBAction)attemptLogin:(id)sender;
 
 @end
