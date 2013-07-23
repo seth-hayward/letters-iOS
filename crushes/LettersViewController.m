51749af51ab5908363ccb64c3d47b07fac1cc4ca
Seth Hayward <seth.hayward@gmail.com>
2013-07-23 11:44:52 -0700

    adding open drawer action for integration with facebook menu, navigation item stuff

--- crushes/LettersViewController.m
+++ crushes/LettersViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "LettersViewController.h"
+#import "MMDrawerBarButtonItem.h"
+#import "AppDelegate.h"
 
 @implementation LettersViewController
 
@@ -14,7 +16,15 @@
 {
     self = [super initWithStyle:UITableViewStyleGrouped];
     if(self) {
+
+        UIBarButtonItem *button_refresh = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshOriginalPage)];
+        [[self navigationItem] setRightBarButtonItem:button_refresh];
         
+        MMDrawerBarButtonItem * leftDrawerButton = [[MMDrawerBarButtonItem alloc] initWithTarget:self action:@selector(openDrawer:)];
+        [self.navigationItem setLeftBarButtonItem:leftDrawerButton animated:YES];
+        
+        [[self navigationItem] setTitle:@"letters to crushes"];
+
     }
     return self;
 }
@@ -23,5 +33,14 @@
 {
     return [self init];
 }
+
+- (void)openDrawer:(id)sender {
+    
+    // now tell the web view to change the page
+    AppDelegate *appDelegate = (AppDelegate *) [[UIApplication sharedApplication] delegate];
+    [appDelegate.drawer toggleDrawerSide:MMDrawerSideLeft animated:YES completion:nil];
+    
+}
+
 
 @end
