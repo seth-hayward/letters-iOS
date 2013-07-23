8fdf2819528321e00754974e4cd6816c4e0f2713
Seth Hayward <seth.hayward@gmail.com>
2013-07-23 13:16:28 -0700

    using a nib, custom class to display a custom uitableviewcell

---
+++ crushes/LetterItemCell.h
@@ -0,0 +1,13 @@
+//
+//  LetterItemCell.h
+//  crushes
+//
+//  Created by Seth Hayward on 7/23/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LetterItemCell : UITableViewCell
+
+@end
---
+++ crushes/LetterItemCell.m
@@ -0,0 +1,13 @@
+//
+//  LetterItemCell.m
+//  crushes
+//
+//  Created by Seth Hayward on 7/23/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "LetterItemCell.h"
+
+@implementation LetterItemCell
+
+@end
