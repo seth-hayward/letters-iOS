3a7651c6999a68cc2291c01b6d7a9e2c29fc373e
Seth Hayward <seth.hayward@gmail.com>
2013-07-24 11:01:46 -0700

    starting to use a scrollview instead of a tableviewcontroller

---
+++ crushes/LettersScrollController.h
@@ -0,0 +1,14 @@
+//
+//  LettersScrollController.h
+//  crushes
+//
+//  Created by Seth Hayward on 7/24/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GAITrackedViewController.h"
+
+@interface LettersScrollController : GAITrackedViewController
+
+@end
---
+++ crushes/LettersScrollController.m
@@ -0,0 +1,13 @@
+//
+//  LettersScrollController.m
+//  crushes
+//
+//  Created by Seth Hayward on 7/24/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "LettersScrollController.h"
+
+@implementation LettersScrollController
+
+@end
