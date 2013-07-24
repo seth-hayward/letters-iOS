bc4f33229ba42caf573ee11f1c90aef795b69b8d
Seth Hayward <seth.hayward@gmail.com>
2013-07-24 11:07:59 -0700

    configuring navigation bar for new view, having it load the letters here

--- crushes/LettersScrollController.m
+++ crushes/LettersScrollController.m
@@ -7,7 +7,28 @@
 //
 
 #import "LettersScrollController.h"
+#import "MMDrawerBarButtonItem.h"
+#import "RODItemStore.h"
 
 @implementation LettersScrollController
+
+- (id)init
+{
+    self = [super init];
+    if(self) {
+        
+        UIBarButtonItem *button_refresh = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshOriginalPage)];
+        [[self navigationItem] setRightBarButtonItem:button_refresh];
+        
+        MMDrawerBarButtonItem * leftDrawerButton = [[MMDrawerBarButtonItem alloc] initWithTarget:self action:@selector(openDrawer:)];
+        [self.navigationItem setLeftBarButtonItem:leftDrawerButton animated:YES];
+        
+        [[self navigationItem] setTitle:@"letters to crushes"];
+        
+        [[RODItemStore sharedStore] loadLettersByPage:1 level:0];
+        
+    }
+    return self;
+}
 
 @end
