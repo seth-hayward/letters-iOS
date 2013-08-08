3170301e15e8d27ef4519106bfa2bba3671448ea
Seth Hayward <seth.hayward@gmail.com>
2013-08-08 13:39:13 -0700

    adding starting point for comment item

---
+++ crushes/CommentScrollViewItem.h
@@ -0,0 +1,13 @@
+//
+//  CommentScrollViewItem.h
+//  crushes
+//
+//  Created by Seth Hayward on 8/8/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentScrollViewItem : UIViewController
+
+@end
---
+++ crushes/CommentScrollViewItem.m
@@ -0,0 +1,38 @@
+//
+//  CommentScrollViewItem.m
+//  crushes
+//
+//  Created by Seth Hayward on 8/8/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "CommentScrollViewItem.h"
+
+@interface CommentScrollViewItem ()
+
+@end
+
+@implementation CommentScrollViewItem
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
