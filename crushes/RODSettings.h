e7d120d4b406e999bd0d8caedb3ea91f99e07949
Seth Hayward <seth.hayward@gmail.com>
2013-08-19 13:25:46 -0700

    adding a new object to serialize and keep track of user settings

---
+++ crushes/RODSettings.h
@@ -0,0 +1,13 @@
+//
+//  RODSettings.h
+//  crushes
+//
+//  Created by Seth Hayward on 8/19/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RODSettings : NSObject
+
+@end
---
+++ crushes/RODSettings.m
@@ -0,0 +1,13 @@
+//
+//  RODSettings.m
+//  crushes
+//
+//  Created by Seth Hayward on 8/19/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "RODSettings.h"
+
+@implementation RODSettings
+
+@end
