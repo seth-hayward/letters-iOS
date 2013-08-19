c4dce49a6118da3ba95df20c2fcdf5574ac6310a
Seth Hayward <seth.hayward@gmail.com>
2013-08-19 14:02:29 -0700

    calling the save function when the enter enters the background

--- crushes/AppDelegate.m
+++ crushes/AppDelegate.m
@@ -83,6 +83,13 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    BOOL success = [[RODItemStore sharedStore] saveSettings];
+    if (success) {
+        NSLog(@"Saved settings.");
+    } else {
+        NSLog(@"Error saving settings.");
+    }
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
