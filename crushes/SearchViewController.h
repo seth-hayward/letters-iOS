0d2e93c8a8879e4a97b16fe78ce32000faa9306f
Seth Hayward <seth.hayward@gmail.com>
2013-08-20 12:14:08 -0700

    adding beggining of the search view controller

---
+++ crushes/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  crushes
+//
+//  Created by Seth Hayward on 8/20/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ crushes/SearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.m
+//  crushes
+//
+//  Created by Seth Hayward on 8/20/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
