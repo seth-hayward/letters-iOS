acc5fc2cf7943daff7d5eedc7c13b60524d8605a
Seth Hayward <seth.hayward@gmail.com>
2013-08-21 17:01:16 -0700

    fix comment height, was getting cut off

--- crushes/LetterCommentsViewController.m
+++ crushes/LetterCommentsViewController.m
@@ -163,7 +163,7 @@
         // the height of the padding around the
         // heart button and the frame of the scrollviewitem is about 40px.
         
-        scv.view.frame = CGRectMake(0, yOffset, self.view.bounds.size.width - 5, comment_height + 50);
+        scv.view.frame = CGRectMake(0, yOffset, self.view.bounds.size.width - 5, comment_height + 65);
         
         //        [scv.webView setDelegate:self];
         //        [scv.webView setDelegate:scv.view];
@@ -175,7 +175,7 @@
         
         [scv setCurrent_comment:full_comment];
         
-        yOffset = yOffset + (comment_height + 50);
+        yOffset = yOffset + (comment_height + 65);
         
         [self.scrollView addSubview:scv.view];
         
