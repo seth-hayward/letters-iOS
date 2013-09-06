e28c434e2b3139b9f95a9b75f5913d7018634286
Seth Hayward <seth.hayward@gmail.com>
2013-09-06 12:22:47 -0700

    adding initial chat name controller, step before chatting

---
+++ crushes/ChatNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatNameViewController.h
+//  crushes
+//
+//  Created by Seth Hayward on 9/6/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatNameViewController : UIViewController
+
+@end
---
+++ crushes/ChatNameViewController.m
@@ -0,0 +1,38 @@
+//
+//  ChatNameViewController.m
+//  crushes
+//
+//  Created by Seth Hayward on 9/6/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "ChatNameViewController.h"
+
+@interface ChatNameViewController ()
+
+@end
+
+@implementation ChatNameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
