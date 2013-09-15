3965d369fa004c87765339d2d3a671e69dfbaa57
Seth Hayward <seth.hayward@gmail.com>
2013-09-15 10:23:02 -0700

    working on add comment interface

---
+++ crushes/AddCommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCommentViewController.h
+//  crushes
+//
+//  Created by Seth Hayward on 9/15/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCommentViewController : UIViewController
+
+@end
---
+++ crushes/AddCommentViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddCommentViewController.m
+//  crushes
+//
+//  Created by Seth Hayward on 9/15/13.
+//  Copyright (c) 2013 Seth Hayward. All rights reserved.
+//
+
+#import "AddCommentViewController.h"
+
+@interface AddCommentViewController ()
+
+@end
+
+@implementation AddCommentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
